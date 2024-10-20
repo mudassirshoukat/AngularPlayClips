f403fc81a050c2facd075012a10893a3b12802d5
mudassirshoukat <mudassirshoukat7577@gmail.com>
2024-10-20 21:44:32 +0500

    created post Component

---
+++ src/app/_Components/post/post.component.ts
@@ -0,0 +1,50 @@
+import { AfterContentChecked, AfterContentInit,AfterViewChecked, AfterViewInit,
+    Component, DoCheck, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit, OnChanges, DoCheck, 
+AfterContentInit, AfterContentChecked,
+AfterViewInit, AfterViewChecked, OnDestroy {
+  @Input('img') postImg = ''
+  @Output() imgSelected = new EventEmitter<string>()
+
+  constructor() {
+    console.log('constructor() called', this.postImg)
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit() called', this.postImg)
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges() called')
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck() called')
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked() called')
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit() called')
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked() called')
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit() called')
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy() called')
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: `<p>Hello world!</p>`,
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
-  template:`<p>Hello world</p>`
+  styleUrls: ['./app.component.css'],
+  // styles: ['']
 })
 export class AppComponent {
-  title = 'AngularPlayClips';
+  name = 'Luis'
+  imgURL = 'https://picsum.photos/id/237/500/500'
+
+  getName() {
+    return this.name
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value
+  }
+
+  logImg(event: string) {
+    console.log(event)
+  }
 }
